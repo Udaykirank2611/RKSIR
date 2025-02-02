53fdcd1b877a84d5f2ec395560fa2fff903ee028
Uday kiran <76544318+Udaykirank2611@users.noreply.github.com>
2025-02-02 22:51:12 +0530

    Update abc.py

--- abc.py
+++ abc.py
@@ -2,3 +2,4 @@ import streamlit as st
 st.title("View PDF from Google Drive")
 pdf_url = "https://drive.google.com/file/d/1Ey7_UDgxJWoy7ZEzp3ulfXkRrtBGtHZT/preview"
 pdf_display = f'<iframe src="{pdf_url}" width="700" height="1000" type="application/pdf"></iframe>'
+st.markdown(pdf_display, unsafe_allow_html=True)
